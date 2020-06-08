37465d5977739bb44a6699063a3c8dfe43ef2106
ku222 <44293915+ku222@users.noreply.github.com>
2020-06-08 10:50:47 +0100

    Updated CardBuilder

--- NavigationBranchFormatter/__init__.py
+++ NavigationBranchFormatter/__init__.py
@@ -5,6 +5,7 @@ import requests
 import pandas as pd
 import json
 import io
+from adaptivecardbuilder import *
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     '''
@@ -114,210 +115,73 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             # Add to list
             banks_list.append(bank)
             
-        sorted_banks_list = sorted(banks_list, key=lambda bank: bank.distance, reverse=True)
+        sorted_banks_list = sorted(banks_list, key=lambda bank: bank.distance, reverse=False)
         return sorted_banks_list[:3]
     
     ###############################################################
-    ## Programmatically create our adaptive card
+    ## Programmatically create adaptive card using our home-grown adaptivecardbuilder API
     ###############################################################
     def create_card(banks_list):
-        # Initialize Card schema
-        card = {
-            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-            "type": "AdaptiveCard",
-            "version": "1.0",
-            "body": [
-                {
-                    "type": "Container",
-                    "items": []
-                }
-            ]
-        }
+        # initialize our card
+        card = AdaptiveCard()
+        # loop over branches - each one will have a mini-card to itself
+        for bank in banks_list:
+            card.add(TextBlock(text=f"{bank.distance} miles away", separator="true", spacing="large"))
+            card.add(ColumnSet())
+            
+            # First column - bank info
+            card.add(Column(width=2))
+            card.add(TextBlock(text="BANK OF LINGFIELD BRANCH"))
+            card.add(TextBlock(text=bank.name, size="ExtraLarge", weight="Bolder", spacing="None"))
+            card.add(TextBlock(text="5 stars", isSubtle=True, spacing="None"))
+            card.add(TextBlock(text=f"Opens at {str(bank.opening_time).zfill(4)}", isSubtle=True, spacing="None"))
+            card.add(TextBlock(text=f"Closes at {str(bank.closing_time).zfill(4)}", isSubtle=True, spacing="None"))
+            card.add(TextBlock(text="**Matt H. said** Im compelled to give this place 5 stars due to the number of times Ive chosen to bank here this past year!", size="Small", wrap="true"))
+            
+            card.up_one_level() # Back up to column set
+            
+            # Second column - image
+            card.add(Column(width=1))
+            img = "https://s17026.pcdn.co/wp-content/uploads/sites/9/2018/08/Business-bank-account-e1534519443766.jpeg"
+            card.add(Image(url=img))
+            
+            # Back to card's body container
+            card.back_to_top()
         
-        card_items = card['body'][0]['items']
-        
-        # Add items
-        for (i, bank) in enumerate(banks_list):
-            all_elements = []
-            # Header + Separator
-            header = {
-                "type": "TextBlock",
-                "text": f"{bank.distance} miles away",
-                "spacing": "large",
-                "separator": "True"
-            }
-            # Initialize the columns set
-            column_set = {
-                "type": "ColumnSet",
-                "columns": []
-            }
-            
-            # Now for 2-width column
-            for _ in range(1):
-                column_width_2 = {
-                    "type": "Column",
-                    "width": 2,
-                    "items": []
-                }
-                # Add elements to 2-width column
-                for _ in range(1): 
-                    # Add title to the 2-width column
-                    column_width_2['items'].append({
-                        "type": "TextBlock",
-                        "text": "BANK OF LINGFIELD BRANCH"
-                    })
-                    # Add name of the branch in bold
-                    column_width_2['items'].append({
-                        "type": "TextBlock",
-                        "text": f"{bank.name}",
-                        "weight": "Bolder",
-                        "size": "ExtraLarge",
-                        "spacing": "None"
-                    })
-                    # Add rating
-                    column_width_2['items'].append({
-                        "type": "TextBlock",
-                        "text": "4.2 Stars",
-                        "isSubtle": "True",
-                        "spacing": "None"
-                    })
-                    # Add review
-                    column_width_2['items'].append({
-                        "type": "TextBlock",
-                        "text": "**Matt H. said** Im compelled to give this place 5 stars due to the number of times Ive chosen to bank here this past year!",
-                        "size": "Small",
-                        "wrap": "True"
-                    })
-                    
-            # Now for the 1-width column
-            for _ in range(1):
-                column_width_1 = {
-                    "type": "Column",
-                    "width": 1,
-                    "items": []
-                }
-                # Add image to the column
-                for _ in range(1):
-                    column_width_1['items'].append({
-                        "type": "Image",
-                        "url": "https://s17026.pcdn.co/wp-content/uploads/sites/9/2018/08/Business-bank-account-e1534519443766.jpeg",
-                        "size": "auto",
-                        "altText": "Seated guest drinking a cup of coffee"
-                        })
-                    
-            # Add 2-width and 1-width columns to the column set (2 -> 1)
-            column_set['columns'].append(column_width_2)
-            column_set['columns'].append(column_width_1)
-            
-            # Add header and column set to all container
-            all_elements.append(header)
-            all_elements.append(column_set)
-            
-            #################################################################
-            # Now for actions
-            action_set = {
-                "type": "ActionSet",
-                "actions": []
-            }
-            
-            # Add show me on a map button to actions set
-            show_me_on_map_button = {
-                "type": "Action.OpenUrl",
-                "title": "Show me on a map!",
-                "url": "https://adaptivecards.io"
-            }
-            
-            # action set has an expandible "show card"
-            showcard = {
-                "type": "Action.ShowCard",
-                "title": "Show Available Appointments",
-                "card": {
-                    "type": "AdaptiveCard",
-                    "body": []
-                }
-            }
+            # add action set to contain our interactive elements
+            card.add(ActionSet())
 
-            # add actions to the show card's body - one per appointment
-            showcard_body = showcard['card']['body']
+            # First add our "View on Map" button
+            card.add(ActionOpenUrl(url=f"https://www.google.com/maps/@{bank.lat},{bank.lon},5.33z", title="View on Map"), is_action=True)
             
-            for (j, appointment) in enumerate(bank.appointments):
-                showcard_column_set = {
-                    "type": "ColumnSet",
-                    "style": "emphasis",
-                    "columns": []
-                }
-                
-                showcard_column_set_columns = showcard_column_set['columns']
-                # Add Appointment margin
-                showcard_column_set_columns.append({
-                    "type": "Column",
-                    "style": "emphasis",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": f"Appointment {j+1}"
-                        }
-                    ],
-                    "bleed": "true",
-                    "width": "auto"
-                })
-                
-                # Add start time label
-                showcard_column_set_columns.append({
-                    "type": "Column",
-                    "style": "emphasis",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": f"{appointment.start_time}"
-                        }
-                    ],
-                    "width": "auto"
-                })
-                
-                # Add end time label
-                showcard_column_set_columns.append({
-                    "type": "Column",
-                    "style": "emphasis",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": f"{appointment.end_time}"
-                        }
-                    ],
-                    "width": "auto"
-                })
-                
-                # Add appointment booking button
-                showcard_column_set_columns.append({
-                    "type": "Column",
-                    "width": "auto",
-                    "items": [
-                        {
-                            "type": "ActionSet",
-                            "actions": [
-                                {
-                                    "type": "Action.Submit",
-                                    "title": "Book This!"
-                                }
-                            ]
-                        }
-                    ]
-                })
-                
-                # Add column set to showcard body
-                showcard_body.append(showcard_column_set)
-                
-            # Add the above actions to the action set
-            action_set['actions'].append(showcard)
-            action_set['actions'].append(show_me_on_map_button)
+            # create expandible card to show all our bank-specific appointment items
+            card.add(ActionShowCard(title="View Appointments"), is_action=True)
             
-            # Add to all elements
-            all_elements.append(action_set)
-            card_items.extend(all_elements)
-            
-        return card
+            # Save a checkpoint at this level to come back to later
+            action_showcard_level = card.save_level()
 
+            # now loops over appointment items and add them
+            for appointment in bank.appointments:
+                card.add(ColumnSet())
+                
+                # Add our slots, start, end times
+                row_items = ["Slot", appointment.start_time, appointment.end_time]
+                for item in row_items:
+                    card.add(Column(style="emphasis", verticalContentAlignment="Center"))
+                    card.add(TextBlock(text=item, horizontalAlignment="Center"))
+                    card.up_one_level() # Back to column set level
+
+                # Add the "Book This!" button, in the final column
+                card.add(Column(style="emphasis", verticalContentAlignment="Center"))
+                card.add(ActionSet())
+                card.add(ActionSubmit(title="Book this!", data={"Appt": f"({appointment.start_time}, {appointment.end_time})"}), is_action=True)
+                card.load_level(action_showcard_level) # back to showcard's body
+            
+            # Go back to the main body of the card, ready for next branch
+            card.back_to_top()
+            
+        return card.to_json()
+    
     ###############################################################
     ## MAIN
     ###############################################################
@@ -326,5 +190,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     card = create_card(banks_list)
     
     # Return OK http response
-    return func.HttpResponse(body=json.dumps(card), status_code=200)
+    return func.HttpResponse(body=card, status_code=200)
 
--- NavigationBranchFormatter/deployed_testing.py
+++ NavigationBranchFormatter/deployed_testing.py
@@ -3,11 +3,15 @@
 
 import requests
 
-DEPLOYED_URL = 'http://cloudwars2functionapp.azurewebsites.net/api/ValidatePINStrength?code=SsjG8i3VjwFGEJyZKuEh5g/rHzu3thZWO4bFUiPTB19HM0iNIGFogQ=='
-json_payload = {'NI': '4490'}
+DEPLOYED_URL = 'http://cloudwars2functionapp.azurewebsites.net/api/NavigationBranchFormatter?code=GMtryo1P7lJD3Jq4K7ILowWbTC/HKfM97dZ6bNLqdV3e9xgISHCftw=='
+
+json_payload = {'address': 'Beverly Hills, 90210'}
 response = requests.post(url=DEPLOYED_URL, json=json_payload)
-response.text
 
 
 #%%
 
+response.text
+
+
+#%%
